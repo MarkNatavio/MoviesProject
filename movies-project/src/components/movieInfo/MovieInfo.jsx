3ac6c8d495723c9dde2190bb288f491c75b889df
Mark Natavio <marknatavio@Marks-MacBook-Pro.local>
2021-01-13 18:56:41 -0500

    Add ons

---
+++ movies-project/src/components/movieInfo/MovieInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function MovieInfo(){
+    return (
+        <h1>Movie Info Page</h1>
+    )
+}
---
+++ movies-project/src/service/index.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+
+// Setting up URLs for data collected
+const apiKey = 'API_KEY'; // API key
+const URL = 'https://api.themoviedb.org/3'; // API website URL
+const nowPlayingURL = '${URL}/movie/now_playing'; // current playing trailer URL
+const topRatedURL = '${URL}/movie/top_rated'; //  DB URL of top rated movies
+const movieURL = '${URL}/movie'; // DB URL of movie info
+const genreURL = '${URL}/genre/movie/list'; // DB URL of movies of specific genre
+const moviesURL = '${URL}/discover/movie'; // DB URL of movies similar to a movie
+const personURL = '${URL}/trending/person/week'; // WONT USE PROBABLY
+
+// Get info of movie selected
+export const fetchMovies = async()=> {
+    try {
+        const { data } = await axios.get(nowPlayingURL, {
+            params: {
+                API_KEY: apiKey,
+                language: 'en_US',
+                page: 1
+            }
+        })
+        const posterUrl = 'https://image.tmdb.org/t/p/original/';
+        const modifiedData = data['results'].map((m) => ({
+            id: m['id'],
+            backPoster: posterUrl + m['backdrop_path'],
+            popularity: m['popularith'],
+            title: m['title'],
+            poster: posterUrl + m['poster_path'],
+            overview: m['overview'],
+            rating: m['vote_average'],
+        }))
+        return modifiedData;
+
+    } catch (error){ }
+}
+
+export const fetchGenre = ()=> {
+    try {
+
+    } catch (error) { }
+}
+
+export const fetchMoviesOfGenre = ()=> {
+    try {
+
+    } catch (error) { }
+}
+
+export const fetchPersonsInfo = ()=> {
+    try {
+
+    } catch (error) { }
+}
+
+export const fetchTopMovies = ()=> {
+    try {
+
+    } catch (error) { }
+}
+
+export const fetchMovieInfo = ()=> {
+    try {
+
+    } catch (error) { }
+}
+
+export const fetchMovieVids = ()=> {
+    try {
+
+    } catch (error) { }
+}
+
+export const fetchCastMembers = ()=> {
+    try {
+
+    } catch (error) { }
+}
+
+export const fetchSimilarMovies = ()=> {
+    try {
+
+    } catch (error) { }
+}
